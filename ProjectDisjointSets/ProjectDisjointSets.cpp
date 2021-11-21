7c513dedb88c0e689db614c950e401869ad08b9f
Michal Swistowski <minauka@tuta.io>
2021-11-21 18:35:59 +0100

    tests_michal

--- ProjectDisjointSets/ProjectDisjointSets.cpp
+++ ProjectDisjointSets/ProjectDisjointSets.cpp
@@ -1,70 +1,70 @@
 ﻿#include <iostream>
 #include "DisjointSetBase.h"
 
-template<typename T>
-struct Node
-{
-    T el;
-    Node* next;
-    Node* root;
-};
-template<typename T>
-struct NodeForset
-{
-    T el;
-    Node* el;
-    Node* root;
-};
-
-template<typename T>
-class DisjointList : DisjointSetBase<T, Node<T>*>
-{
-public:
-    // Odziedziczono za pośrednictwem elementu DisjointSetBase
-    virtual Node<T>* Find(Node<T>* t) override
-    {
-
-    }
-
-    virtual void Union(Node<T>* x, Node<T>* y) override
-    {
-
-    }
-
-    virtual Node<T>* MakeSet(T el) override
-    {
-
-    }
-
-};
-
-template<typename T>
-class DisjointArray : DisjointSetBase<T, int>
-{
-public:
-    // Odziedziczono za pośrednictwem elementu DisjointSetBase
-    virtual int Find(int t) override
-    {
-
-    }
-
-    virtual void Union(int x, int y) override
-    {
-
-    }
-
-    virtual int MakeSet(T el) override
-    {
-
-    }
-
-};
+//template<typename T>
+//struct Node
+//{
+//    T el;
+//    Node* next;
+//    Node* root;
+//};
+//template<typename T>
+//struct NodeForset
+//{
+//    T el;
+//    Node* el;
+//    Node* root;
+//};
+//
+//template<typename T>
+//class DisjointList : DisjointSetBase<T, Node<T>*>
+//{
+//public:
+//    // Odziedziczono za pośrednictwem elementu DisjointSetBase
+//    virtual Node<T>* Find(Node<T>* t) override
+//    {
+//
+//    }
+//
+//    virtual void Union(Node<T>* x, Node<T>* y) override
+//    {
+//
+//    }
+//
+//    virtual Node<T>* MakeSet(T el) override
+//    {
+//
+//    }
+//
+//};
+//
+//template<typename T>
+//class DisjointArray : DisjointSetBase<T, int>
+//{
+//public:
+//    // Odziedziczono za pośrednictwem elementu DisjointSetBase
+//    virtual int Find(int t) override
+//    {
+//
+//    }
+//
+//    virtual void Union(int x, int y) override
+//    {
+//
+//    }
+//
+//    virtual int MakeSet(T el) override
+//    {
+//
+//    }
+//
+//};
 
 int main()
 {
-    DisjointList<int> XD;
+    /*DisjointList<int> XD;
     auto* ptr = XD.MakeSet(1);
     DisjointArray<int> XD;
 
-    auto index = XD.MakeSet(1);
+    auto index = XD.MakeSet(1);*/
 }
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,6 +1,11 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+namespace Tests
+{
+	TEST(TestCaseName, TestName) {
+		EXPECT_EQ(1, 1);
+		EXPECT_TRUE(true);
+		//EXPECT_EQ(1, 2);
+	}
 }
